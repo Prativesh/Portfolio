9db02770f6df3f45df0d5656572f6f7e57d26e86
Prativesh <aryaprativesh@gmail.com>
2024-08-15 13:17:15 +0530

    Adding key for iterator

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -15,9 +15,8 @@ const Footer = () => {
       <div className="flex mt-16 md:flex-row flex-col justify-center items-center">
         <div className="flex items-center gap-10">
           {socialMedia.map((info) => (
-            <a href={info.link} target="_blank">
+            <a href={info.link} target="_blank" key={info.id}>
             <div
-              key={info.id}
               className="w-20 h-20 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300"
             >
               <img src={info.img} alt="icons" width={20} height={20} />
